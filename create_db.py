513b79b2914e0cc5d94e281e12964ab529a3feba
daniel <4aba3691@gmail.com>
2024-06-15 23:00:27 +0200

    adding the count of books to test_data.json

--- create_db.py
+++ create_db.py
@@ -6,8 +6,9 @@ def main():
     engine = DBManager.create_engine_file(TestDataGet.get_config_data('data_base_path'))
     DBManager.create_tables(engine)
     session = DBManager.create_session(engine)
+    count_of_books = TestDataGet.get_test_data('count_of_books')
 
-    DBManager.populate_db(session, num_books=10)
+    DBManager.populate_db(session, num_books=count_of_books)
 
     print("Database created and populated with initial data.")
 
